b577b2d12f0438e0ff160b859130be64d29da33a
Miguel Albuquerque <migalb5@gmail.com>
2025-04-25 17:01:39 +0200

    Introduction of transactional control.

--- tests/testthat/test-insert_new_data.R
+++ tests/testthat/test-insert_new_data.R
@@ -8,6 +8,7 @@ test_that("A set of financial records is correctly appended to the DB", {
   index_ts2 = "meta_platforms_meta"
 
   conn <- connect_db()
+  DBI::dbBegin(conn)
   batch <- data.frame(symbol = c("AAPL", "META"), index_ts = c("apple_inc_aapl", "meta_platforms_meta"))
   batch_chunks <- split_batch(batch, 5)
   for (chunk in batch_chunks) {
@@ -16,13 +17,17 @@ test_that("A set of financial records is correctly appended to the DB", {
     rows_inserted <- insert_new_data(conn, 0, formatted_data)
     expect_gte(rows_inserted, 1)
   }
+  DBI::dbCommit(conn)
+  DBI::dbBegin(conn)
   query = glue::glue_sql("DELETE FROM student_miguel.data_sp500
                           WHERE date = {date_from} AND
                          (index_ts = {index_ts1} OR index_ts = {index_ts2})", .con = conn)
   DBI::dbExecute(conn, query)
+  DBI::dbCommit(conn)
 #  DBI::dbDisconnect(conn)
 
 #  conn <- connect_db()
+  DBI::dbBegin(conn)
   batch <- data.frame(symbol = c("AAPL", "META"), index_ts = c("apple_inc_aapl", "meta_platforms_meta"))
   batch_chunks <- split_batch(batch, 5)
   for (chunk in batch_chunks) {
@@ -31,13 +36,17 @@ test_that("A set of financial records is correctly appended to the DB", {
     rows_inserted <- insert_new_data(conn, 285, formatted_data)
     expect_gte(rows_inserted, 1)
   }
+  DBI::dbCommit(conn)
+  DBI::dbBegin(conn)
   query = glue::glue_sql("DELETE FROM student_miguel.data_sp500
                           WHERE date = {date_from} AND
                          (index_ts = {index_ts1} OR index_ts = {index_ts2})", .con = conn)
   DBI::dbExecute(conn, query)
+  DBI::dbCommit(conn)
 #  DBI::dbDisconnect(conn)
 
 #  conn <- connect_db()
+  DBI::dbBegin(conn)
   batch <- data.frame(symbol = c("AAPL", "META"), index_ts = c("apple_inc_aapl", "meta_platforms_meta"))
   batch_chunks <- split_batch(batch, 5)
   for (chunk in batch_chunks) {
@@ -46,12 +55,15 @@ test_that("A set of financial records is correctly appended to the DB", {
     rows_inserted <- insert_new_data(conn = conn, formatted_data = formatted_data)
     expect_gte(rows_inserted, 1)
   }
+  DBI::dbCommit(conn)
+  DBI::dbBegin(conn)
   query = glue::glue_sql("DELETE FROM student_miguel.data_sp500
                           WHERE date = {date_from} AND
                          (index_ts = {index_ts1} OR index_ts = {index_ts2})", .con = conn)
   DBI::dbExecute(conn, query)
+  DBI::dbCommit(conn)
   DBI::dbDisconnect(conn)
-  Sys.sleep(5)
+#  Sys.sleep(5)
 })
 
 
@@ -63,6 +75,7 @@ test_that("A set of financial records is not appended to the DB because they are
   index_ts2 = "meta_platforms_meta"
 
   conn <- connect_db()
+  DBI::dbBegin(conn)
   batch <- data.frame(symbol = c("AAPL", "META"), index_ts = c("apple_inc_aapl", "meta_platforms_meta"))
   batch_chunks <- split_batch(batch, 5)
   for (chunk in batch_chunks) {
@@ -71,10 +84,12 @@ test_that("A set of financial records is not appended to the DB because they are
     rows_appended <- insert_new_data(conn, 0, formatted_data)
     expect_equal(rows_appended, nrow(batch) * 5 * (27-26))
   }
+  DBI::dbCommit(conn)
 #  DBI::dbDisconnect(conn)
 #  Sys.sleep(10)
 
 #  conn <- connect_db()
+  DBI::dbBegin(conn)
   batch <- data.frame(symbol = c("AAPL", "META"), index_ts = c("apple_inc_aapl", "meta_platforms_meta"))
   batch_chunks <- split_batch(batch, 5)
   for (chunk in batch_chunks) {
@@ -83,10 +98,13 @@ test_that("A set of financial records is not appended to the DB because they are
     rows_appended <- insert_new_data(conn, 0, formatted_data)
     expect_equal(rows_appended, 0)
   }
+  DBI::dbCommit(conn)
+  DBI::dbBegin(conn)
   query = glue::glue_sql("DELETE FROM student_miguel.data_sp500
                           WHERE date = {date_from} AND
                          (index_ts = {index_ts1} OR index_ts = {index_ts2})", .con = conn)
   DBI::dbExecute(conn, query)
+  DBI::dbCommit(conn)
   DBI::dbDisconnect(conn)
 
 
